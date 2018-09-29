62a37ce68ed28bc7761721ed86706447bb11db1e
TiagoDanin <TiagoDanin@outlook.com>
2018-09-29 11:41:50 -0300

    Init bot

--- index.js
+++ index.js
@@ -3,17 +3,16 @@
  * @param {import('probot').Application} app - Probot's Application class.
  */
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
-
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+	app.log('Yay, the app was loaded!')
+	app.on('issues.opened', async (ctx) => {
+		return ctx.github.issues.creLabels(
+			ctx.issue({
+				labels: [{
+					name: 'Hacktoberfest',
+					description: 'https://hacktoberfest.digitalocean.com',
+					color: '#21ffff'
+				}]
+			})
+		)
+	})
 }
