10097e3dd6a61fc82b9e6ec5ab76af5894a50a7f
aliasgharkarani <aliasgharkarani125@hotmail.com>
2019-10-07 23:52:16 +0500

    Grammer Fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @param {import('probot').Application} app - Probot's Application class.
  */
 module.exports = app => {
-	app.log('Yay, the app was loaded!')
+	app.log('Yay, the app is loaded!')
 	app.on('issues.opened', async (ctx) => {
 		return ctx.github.issues.addLabels(
 			ctx.issue({
